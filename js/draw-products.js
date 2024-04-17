4dd9b63621cc97d9ceee45c3ef63cf27a41d7379
Bryan Urbina <bryanguevara38@gmail.com>
2024-04-17 14:08:53 -0600

    SE ESTA MEJORANDO EL DISENIO DEL SHOP

--- js/draw-products.js
+++ js/draw-products.js
@@ -1,7 +1,36 @@
+const getData = async (category) => {
 
-const drawProduct = (data) => {
+  let isInteger = Number.isInteger(category);
+
+  return await fetch("./../js/products.JSON")
+    .then(res => res.json())
+    .then((res) => {
+
+      let filter = res.filter(function (res) {
+        // return res.categoria === 'Perecedero';
+
+        if (isInteger) {
+          return res.item === category;
+        } else {
+          if (category === 'Perecedero') {
+            return res.categoria === category;
+          }
+          if (category === 'No perecedero') {
+            return res.categoria === category;
+          }
+          if (category === 'all') {
+            return res;
+          }
+        }
+      });
+      return filter;
+    });
+};
+
+const drawProduct = async (category) => {
   let container = document.getElementById('container-products');
   let cards = '';
+  var data = await getData(category);
 
   data.forEach(e => {
     cards += `<div class="product-card">
@@ -9,12 +38,13 @@ const drawProduct = (data) => {
           <figure>
               <img src="img/products/${e.img}.png" alt="${e.descripcion}">
           </figure>
-                <span class="btn-add" data-item='${e.item}'>+</span>
+          
+            <span class="btn-add id-item btn-add-basket" onclick="insertProduct(this)" data-item='${e.item}'>+</span>
       </div>
       <div class="info-product">
           <p class="p-info">Venta al por mayor</p>
               <span class="price">
-                      ${e.precio} x ${e.udm}
+                      C$${e.precio} x ${e.udm}
               </span>
               <p class="description">
                       ${e.descripcion}
@@ -28,34 +58,14 @@ const drawProduct = (data) => {
   container.innerHTML = cards
 }
 
-
-
-const getData = (category) => {
-  fetch("./../js/products.JSON")
-    .then((res) => res.json())
-    .then((datos) => {
-
-      let filter = datos.filter(function (datos) {
-        // return datos.categoria === 'Perecedero';
-        if (datos.categoria === category) {
-          return datos
-        } else if (datos.categoria === category) {
-          return datos
-        }
-      });
-      console.log(filter)
-      drawProduct(filter)
-    });
-};
-
 // CLICK BTN PERECEDEROS
 
 document.getElementById('perecederos').addEventListener('click', function () {
-  getData('Perecedero');
+  drawProduct('Perecedero');
 })
 
 // CLICK BTN PERECEDEROS
 
 document.getElementById('no-perecederos').addEventListener('click', function () {
-  getData('No perecedero');
+  drawProduct('No perecedero');
 })
